de4fbe622e3b7367889f8e918b90ad720d3715d0
Erickdeop <erick.freck1@gmail.com>
2021-04-04 00:14:53 +0000

    Adding 3rd task

---
+++ 0x00-hello_world/2-printf.c
@@ -0,0 +1,15 @@
+/*
+ * File: 1-puts.c
+ * Auth: Erick de Oliveira Pessoa
+ */
+#include <stdio.h>
+
+/**
+ * main - Prints the task requirement
+ * Return: returns always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
