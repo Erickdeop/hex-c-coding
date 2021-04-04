4feb7f3f03c3ce06af968e99c03b28621cef528a
Erickdeop <erick.freck1@gmail.com>
2021-04-04 00:34:08 +0000

    Adding 4th task

---
+++ 0x00-hello_world/3-size.c
@@ -0,0 +1,19 @@
+/*
+ * File: 3-size.c
+ * Auth: Erick de Oliveira Pessoa
+ */
+#include <stdio.h>
+
+/**
+ * main - Prints the task requirement
+ * Return: returns always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
