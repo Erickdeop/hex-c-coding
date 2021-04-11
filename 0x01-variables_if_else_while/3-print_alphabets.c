7bf7493e82ef9af04c2986c8a58f911b4a21a930
Erickdeop <erick.freck1@gmail.com>
2021-04-11 22:10:07 +0000

    Adding forth task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Erick de Oliveira Pessoa
+ */
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase
+ * Return: returns always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
