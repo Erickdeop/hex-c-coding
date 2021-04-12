8eef455350d6732b0ac290ec0ecc7dbd15d9d71e
Erickdeop <erick.freck1@gmail.com>
2021-04-12 04:06:24 +0000

    Adding ninth task

---
+++ 0x01-variables_if_else_while/8-print_comb.c
@@ -0,0 +1,26 @@
+/*
+ * File: 8-print_comb.c
+ * Auth: Erick de Oliveira Pessoa
+ */
+#include <stdio.h>
+/**
+ * main - Prits digits separated by commas
+ * Return: returns always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n == 9; n++)
+	{
+		putchar((n % 10) + '0');
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
