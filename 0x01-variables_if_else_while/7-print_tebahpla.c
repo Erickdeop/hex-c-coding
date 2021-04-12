13bbfde2df4d3a5e028c22d78c1676f4b2c449e8
Erickdeop <erick.freck1@gmail.com>
2021-04-12 03:55:29 +0000

    Addin eighth task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+/*
+ * File: 7-print-tebahlpa.c
+ * Auth: Erick de Oliveira Pessoa
+ */
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in reverse
+ * Return: returns always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c = 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
