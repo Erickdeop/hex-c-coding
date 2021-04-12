066ae2b388665fd1744c06664e8629d17ecb44f7
Erickdeop <erick.freck1@gmail.com>
2021-04-12 03:43:49 +0000

    Adding seventh task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: Erick de Oliveira Pessoa
+ */
+#include <stdio.h>
+/**
+ * main - Prints numbers from 0 to 10
+ * Return: returns always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n)
+	}
+
+	return (0);
+}
