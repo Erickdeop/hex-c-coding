40de5a05710dee2675deea0735fa7917d53426d2
Erickdeop <erick.freck1@gmail.com>
2021-05-12 18:40:30 +0000

    Changing header and adding new task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+/*
+ * File: 1-alphabet.c
+ * Auth: Erick de Oliveira Pessoa
+ */
+#include "holberton.h"
+/**
+ * main - Prints the alphabet in lowercase
+ * Return: returns always 0
+ */
+void print_alphabet(void)
+{
+	int n
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+
+	return (0);
+}
