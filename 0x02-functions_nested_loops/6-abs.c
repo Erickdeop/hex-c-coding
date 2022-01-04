1a132359939ae5d4e79df72f88349a34bf8a317e
Erick <erick.freck1@gmail.com>
2022-01-04 09:38:01 -0300

    updated header and added 6th task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+/*
+ * File: 6-sign.c
+ * Auth: Erick de Oliveira Pessoa
+ */
+#include "holberton.h"
+/**
+ *  _abs - given an int, returns the value of its absolute
+ *  @n: number
+ * Return: absolute of n
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return n * (-1);
+	}
+	else
+	{
+		return n;
+	}
+}
