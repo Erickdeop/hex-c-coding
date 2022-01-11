bb9f1fe431c55547174ab1e7e07f8e29be2657a2
Erick <erick.freck1@gmail.com>
2022-01-11 20:42:08 -0300

    Added new task and updated header

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+/*
+ * File: 7-print_last_digit.c
+ * Auth: Erick de Oliveira Pessoa
+ */
+#include "holberton.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: number
+ * Return: value of last digit
+ */
+
+int print_last_digit(int n)
+{
+	int div;
+
+	div = (n % 10);
+
+	if (div < 0)
+	{
+		div = (-1 * div)
+	}
+
+	_putchar(p + '0');
+	return (div);
+}
