0add39ba11f0f8806a4d2fb48f9d1af4f141ec18
Erick <erick.freck1@gmail.com>
2022-01-11 20:57:53 -0300

    added new task and updated header

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+/*
+ * File: 7-print_last_digit.c
+ * Auth: Erick de Oliveira Pessoa
+ */
+#include "holberton.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: number
+ * Return: value of last digit
+ */
+
+int print_last_digit(int n)
+{
+	int hours, minutes;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (minutes = 0; minutes < 60; minutes++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
