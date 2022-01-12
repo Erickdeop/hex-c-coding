5aebacffd08cb8859558883cb9aaa5ce4e261f38
Erick <erick.freck1@gmail.com>
2022-01-11 22:02:46 -0300

    added new task and updated header

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+/*
+ * File: 11-print_to_98.c
+ * Auth: Erick de Oliveira Pessoa
+ */
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints from n to 98
+ * @n: given number
+ * Return: returns each number from n to 98
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n)
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n)
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+	print("\n");
+}
