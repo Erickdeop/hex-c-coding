b19561b931b83285babd3bf967bd879d0e9bafa9
Erick <erick.freck1@gmail.com>
2022-01-11 21:41:40 -0300

    added new task and updated header

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+/*
+ * File: 10-add.c
+ * Auth: Erick de Oliveira Pessoa
+ */
+#include "holberton.h"
+/**
+ * add - adds two integer and returns the result
+ * @n1: number_1
+ * @n2: number_2
+ * Return: returns the sum of two integers
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
