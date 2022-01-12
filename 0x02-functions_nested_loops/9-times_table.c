2ebcae08b9be861ba01fd5a3beda6ada0e14bd7c
Erick <erick.freck1@gmail.com>
2022-01-11 21:29:17 -0300

    added 9th task and uptdated .h

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+/*
+ * File: 9.times_table.c
+ * Auth: Erick de Oliveira Pessoa
+ */
+#include "holberton.h"
+/**
+ * times_table - prints the times table until 9
+ * @row = row, @column = columm, @n = number
+ * Return: return the times table until 9 every line
+ */
+
+void times_table(void)
+{
+	int row, column, n;
+
+	for (row = 0; row < 10; row++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+
+		for (column = 1; column < 10; column++)
+		{
+			n = (row * column);
+			if ((n / 10) > 10)
+			{
+				_putchar((n / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((n % 10) + '0');
+
+			if (column < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
