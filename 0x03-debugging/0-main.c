38429daf04dd6df1ee175f0217a6b1914b91608e
Erick <erick.freck1@gmail.com>
2022-01-12 19:00:29 -0300

    fixed output

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,15 @@
-/*
- * File: 0-main.c
- * Auth: Erick de Oliveira Pessoa
- */
 #include "holberton.h"
+
 /**
  * main - function that prints if integer is positive or negative
  * Return: returns always 0
  */
+
 int main(void)
 {
 	int i;
 
-	i = 98;
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
